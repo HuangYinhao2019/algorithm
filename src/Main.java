69cb8a9dce457022668dc3132677e50496825ea9
liusandao <fzuhyh@foxmail.com>
2020-01-17 13:48:42 +0800

    2020-1-17

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        String s = "()";
+        Stack<Character> t = new Stack<Character>();
+        for(int i = 0;i < s.length();i++){
+            if(t.empty())
+                t.push(s.charAt(i));
+            else if(s.charAt(i) == ')' && t.peek() == '(')
+                t.pop();
+            else if(s.charAt(i) == ']' && t.peek() == '[')
+                t.pop();
+            else if(s.charAt(i) == '}' && t.peek() == '{')
+                t.pop();
+            else
+                t.push(s.charAt(i));
+        }
+        System.out.println("a" + 'b');
+
     }
 }
